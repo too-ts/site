b8af6ae68854789eb5b65e4cfd1cb1aa466ca00a
karel <kareljf@gmail.com>
2026-02-11 20:14:24 +0100

    actual go stuff

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
+	"context"
 	"html/template"
 	"io"
-	"net/http"
+	"log/slog"
 	"os"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v5"
 	"github.com/labstack/echo/v5/middleware"
-	"github.com/labstack/echo/v5/middleware"
-	"github.com/labstack/gommon/log"
 	"golang.org/x/crypto/acme/autocert"
 )
 
@@ -24,7 +23,7 @@ func newTemplate() *Template {
 	}
 }
 
-func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+func (t *Template) Render(w io.Writer, name string, data interface{}, c *echo.Context) error {
 	return t.tmpl.ExecuteTemplate(w, name, data)
 }
 
@@ -38,49 +37,35 @@ func init() {
 }
 
 func main() {
-	debug := true
 	godotenv.Load()
 
 	e := echo.New()
 
-	if debug {
-		e.Logger.SetLevel(log.DEBUG)
-	}
+	e.Logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
 	e.Use(middleware.Recover())
-	e.Use(middleware.Logger())
 	e.Use(middleware.CORS())
 	e.Use(middleware.Secure())
 
 	data := NewData()
 
-	e.Renderer = newTemplate()
 	e.Static("/css", "css")
 
-	e.GET("/", func(c echo.Context) error {
+	e.GET("/", func(c *echo.Context) error {
 		return c.Render(200, "index.html", data)
 	})
 
-	if debug {
-		e.Logger.Fatal(e.Start(":42069"))
-	} else {
-		m := &autocert.Manager{
-			Prompt:     autocert.AcceptTOS,
-			HostPolicy: autocert.HostWhitelist("example.com", "www.example.com"),
-			Cache: autocert.DirCache("/var/www/.cache"),
-			Email:   "kareljf@gmail.com", 
-		}
-		sc := echo.StartConfig{
-			Address:   ":8443",
-			TLSConfig: m.TLSConfig(),
-		}
-		if err := sc.Start(context.Background(), e); err != nil {
-			e.Logger.Error("failed to start server", "error", err)
-		}
-
-		err := e.StartTLS(":443", fullchain, privkey)
-		if err != http.ErrServerClosed {
-			log.Fatal(err)
-		}
+	m := &autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist("toots.dev", "www.toots.dev"),
+		Cache:      autocert.DirCache("/var/www/.cache"),
+		Email:      "kareljf@gmail.com",
+	}
+	sc := echo.StartConfig{
+		Address:   ":8443",
+		TLSConfig: m.TLSConfig(),
+	}
+	if err := sc.Start(context.Background(), e); err != nil {
+		e.Logger.Error("failed to start server", "error", err)
 	}
 }
