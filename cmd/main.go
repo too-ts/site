8e9c6a2dc45b11e9f8f1ef4d526836766667aae7
karel <kareljf@gmail.com>
2026-02-11 20:24:09 +0100

    more stuff

--- cmd/main.go
+++ cmd/main.go
@@ -44,7 +44,7 @@ func main() {
 	e.Logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
 	e.Use(middleware.Recover())
-	e.Use(middleware.CORS())
+	e.Use(middleware.CORS("https://www.toots.dev", "https://toots.dev"))
 	e.Use(middleware.Secure())
 
 	data := NewData()
